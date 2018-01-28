5f1152563b9731ae19905d0fa306bf77056585b4
Mark Franciose <mark.franciose@gmail.com>
2018-01-27 21:26:08 -0600

    food - create/update views & controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,12 +4,21 @@ class FoodsController < ApplicationController
   end
 
   def create
+		@food = Food.create(food_params)
+		redirect_to foods_path
   end
 
   def edit
+		@food = Food.find(params[:id])
   end
 
 	def update
+		@food = Food.find(params[:id])
+		if @food.update_attributes(food_params)
+			redirect_to foods_path
+		else
+			render 'edit'
+		end
 	end
 
   def show
@@ -23,4 +32,10 @@ class FoodsController < ApplicationController
 			@foods = Food.all
 		end
   end
+
+	private
+
+	def food_params
+		params.require(:food).permit(:name, :calories, :protein, :carbs, :fiber)
+	end
 end
